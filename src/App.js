aa8ff34ee38ae6695d56f51842e8b538b481130b
ALEXANDER DUBOVTSEV <alexdubovtsev.it@gmail.com>
2022-10-25 14:05:31 +0200

    Create React-Redux app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from "react-redux/es/exports";
+import './Styles/App.css'
+
+
+// Redux - библиотека для работы с состоянием приложения (некоторое хранилище данных). Бывают ситуации, когда состояния из 1 компонента нужны в другом компоненте (можно вынести в родительскую компоненту и передавать через пропсы, но это затратно и сложно в больших приложениях). Redux же выносит состояния во внешнюю зависимость, и каждый компонент получает данные уже из этого состояния, при этом состояния для 1 компоненты можно использовать в другой + нужно хранить данные и логику для работы с ними отдельно от компонентов.
+// Actions определяют то, как мы изменяем данные (добавить, изменить). Dispatch принимает эти Actions и передает в Reducer, в котором находится вся логика по работе с данными (он знает все действия). Далее Reducer напрямую изменяет состояния, которые можно будет использовать в React-компонентах
+
+// react-redux - предназначен для того, чтобы связать состояния redux с компонентами
 
 function App() {
+
+  const dispatch = useDispatch(); // изменить состояние
+  const cash = useSelector(state => state.cash); // получить состояние (функция, которая принимает состояние, из которого получаем нужную переменную)
+  console.log(cash);
+
+  const addCash = (cash) => {
+    dispatch({type:'ADD_CASH', payload: cash})
+  }
+
+  const getCash = (cash) => {
+    dispatch({type:'GET_CASH', payload: cash})
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{display: "flex", justifyContent: "center", marginBottom: 20, fontSize: 25}}>{cash}</div>
+      <div style={{display: "flex"}}>
+        {/* promp открывает окно с полем ввода в браузере */}
+        <button onClick={() => addCash(Number(prompt()))}>Add money</button>
+        <button onClick={() => getCash(Number(prompt()))}>Get money</button>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from "./App";
+
+// reducer - функция, которая примимает состояние и action = {type: '', payload: '?'} - js-объект с обязательным полем type, который определяет, как состояние будет изменяться. Состояние - объект, массив или примитивное значение, которое хранит данные
+
+// defaultState присваивается, когда пользователь открыл приложение
+const defaultState = {
+  cash: 5,
+};
+
+// Каждый раз, когда экшн будет прокидываться в диспатч, состояние будет изменяться и хранится в store, пока пользователь не обновит страницу или закроет приложение
+const reducer = (state = defaultState, action) => {
+  // производим логику: какой экш проброшен в функцию
+  // отслеживаем тип проброшенного экшна и отрабатываем кейсы
+  switch (
+    action.type // если прилетел экшн с типом, который не обрататываем ни в каком кейсе, возвратим неизмененное состояние
+  ) {
+    case "ADD_CASH":
+      // состояния в редакс неизменяемые, поэтому каждый раз возвращаем новый объект и в него разворачиваем старое состояние с измененным полем
+      return { ...state, cash: state.cash + action.payload };
+
+    case "GET_CASH":
+      return { ...state, cash: state.cash - action.payload };
+
+    default:
+      return state;
+  }
+};
+
+// store - объект, который содержит методы:
+// getState - получить состояние
+// dispatch изменить состояние
+const store = createStore(reducer);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
